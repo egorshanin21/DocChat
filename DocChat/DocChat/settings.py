9280c0c63d4e75685b6227050fd379186e41ddba
egorshanin21 <egorshanin21@gmail.com>
2023-11-19 01:34:41 +0200

    collect static

--- DocChat/DocChat/settings.py
+++ DocChat/DocChat/settings.py
@@ -141,9 +141,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "/static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_URL = "static/"
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
 LOGIN_REDIRECT_URL = "/"
