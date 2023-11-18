c9548c98b51046755c2b016e78891e34a7fdfa37
egorshanin21 <egorshanin21@gmail.com>
2023-11-19 01:19:40 +0200

    collect static

--- DocChat/DocChat/settings.py
+++ DocChat/DocChat/settings.py
@@ -143,7 +143,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "staticfiles")]
-# STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_ROOT = BASE_DIR / "media"
 MEDIA_URL = "/media/"
 LOGIN_REDIRECT_URL = "/"
